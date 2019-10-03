fa1fefe772cb98a28e9ef4c0dc5091ff829d1234
Matheus Barbosa de Oliveira <matheus.barbosa@dcx.ufpb.br>
2019-10-02 21:27:16 -0300

    add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 
 import logo from "./assets/logo.svg";
 
+import Routes from "./routes";
+
 function App() {
   const [email, setEmail] = useState("");
 
@@ -12,30 +14,14 @@ function App() {
     const res = await api.post("/sessions", { email });
     const { _id } = res.data;
 
-    localStorage.setItem('user', _id);
+    localStorage.setItem("user", _id);
   }
 
   return (
     <div className="container">
       <img src={logo} alt="AirCnc" />
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre{" "}
-          <strong>talentos</strong> para sua empresa
-        </p>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-MAIL *</label>
-          <input
-            id="email"
-            type="email"
-            placeholder="Seu melhor e-mail"
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-          />
-          <button className="btn" type="submit">
-            Entrar
-          </button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
