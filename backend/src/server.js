566f3dd72e2b376a066413f6edb19c61a7163810
Matheus Barbosa de Oliveira <matheus.barbosa@dcx.ufpb.br>
2019-10-02 22:05:51 -0300

    update backend for add virtuals on models

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
 
-const routes = require('./routes');
+const routes = require("./routes");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-4tupd.mongodb.net/OmniStackWeek9?retryWrites=true&w=majority', {
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@cluster0-4tupd.mongodb.net/OmniStackWeek9?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+  }
+);
 
 app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(3333);
